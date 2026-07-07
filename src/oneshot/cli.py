23c63022ba09545a11c76c942e3d355d1ab2670a
Johannes Elias <joheli@gmx.net>
2026-07-07 12:52:45 +0200

    pass csv_separator to llm_request; force qid to string in cli.py

--- src/oneshot/cli.py
+++ src/oneshot/cli.py
@@ -13,59 +13,78 @@ from oneshot import __version__
 
 app = typer.Typer()
 
-help_text = f"oneshot version {__version__}, visit https://github.com/joheli/oneshot for help"
+help_text = (
+    f"oneshot version {__version__}, visit https://github.com/joheli/oneshot for help"
+)
 
-@app.callback(invoke_without_command = True, help = help_text)
-def main(config_file: Annotated[Path, typer.Option("-c", "--config", exists=True, readable=True, dir_okay=False)] = Path("oneshot.toml")):
+
+@app.callback(invoke_without_command=True, help=help_text)
+def main(
+    config_file: Annotated[
+        Path, typer.Option("-c", "--config", exists=True, readable=True, dir_okay=False)
+    ] = Path("oneshot.toml"),
+):
 
     cfg = Config.from_toml(config_file)
-    
+
     # records holds csv data rows
     records = []
-    
+
     # writeout holds text data to be written into a file
     # this is only necessary for long responses
     writeout = []
-    
+
     # be friendly
-    #print(f"Welcome friend! This is outshot version {__version__}. Have fun.\n")
-    
-    with Progress(SpinnerColumn(), TextColumn("[progress.description]{task.description}"), transient=True) as progress:
+    # print(f"Welcome friend! This is outshot version {__version__}. Have fun.\n")
+
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
         for qid, rq in process_config(cfg):
-            task = progress.add_task(description=f"processing query identifier '{qid}'", total=None)
-            raw_response, elapsed = measure_time(requests.post,
-                                        url = rq.url, headers = rq.headers, 
-                                        json = rq.json)
-            #print(resp.json())
+            task = progress.add_task(
+                description=f"processing query identifier '{qid}'", total=None
+            )
+            raw_response, elapsed = measure_time(
+                requests.post, url=rq.url, headers=rq.headers, json=rq.json
+            )
+            # print(resp.json())
             responsefun = RESPONSEFUN[cfg.query.target]
             response = responsefun(raw_response.json())
-            
+
             # check length of LLM response
             response_text_llm = response.response_text
-            
+
             # If length of the LLM response is longer than the specified threshold, save response in list `writeout`.
             # `writeout` will later be processed, i.e. contents will be written to file (see below)
             if len(response_text_llm) > cfg.out.response_to_file_length_threshold:
                 # Determine filename to write long LLM response into:
-                response_to_file_filename = cfg.out.response_to_file_filename.replace("~qid~", qid)
+                response_to_file_filename = cfg.out.response_to_file_filename.replace(
+                    "~qid~", str(qid)
+                )
                 # Append dict containing filename and content (= LLM response) to `writeout`
-                writeout.append({
-                    "file": response_to_file_filename,
-                    "content": response.response_text
-                })
+                writeout.append(
+                    {
+                        "file": response_to_file_filename,
+                        "content": response.response_text,
+                    }
+                )
                 # Suggest placeholder text into csv file:
                 response_text_llm = f"see {response_to_file_filename}"
-            
+
             # `record` is the data row to be written to the csv file
-            record = {"Query ID": qid,
-                    "Provider": response.provider,
-                    "Timepoint (UTC)": response.timepoint,
-                    "Model name": response.model_name,
-                    "Response": response_text_llm,
-                    "Elapsed (seconds)": format(elapsed, ".3f"),
-                    "Usage": str(response.usage_flat)}
+            record = {
+                "Query ID": qid,
+                "Provider": response.provider,
+                "Timepoint (UTC)": response.timepoint,
+                "Model name": response.model_name,
+                "Response": response_text_llm,
+                "Elapsed (seconds)": format(elapsed, ".3f"),
+                "Usage": str(response.usage_flat),
+            }
             records.append(record)
-            
+
             # print to "standard out" if so specified
             if cfg.out.mode == "standard":
                 print(f"Model {response.model_name} on {response.provider}:")
@@ -73,18 +92,18 @@ def main(config_file: Annotated[Path, typer.Option("-c", "--config", exists=True
                 print(f"Response: {response.response_text}")
                 print(f"Timepoint (UTC): {response.timepoint}")
                 print(f"Elapsed: {elapsed:.3f} seconds\n")
-                
+
             # remove the line from the progress
             progress.remove_task(task_id=task)
-        
+
         # if output to file specified, write results to csv file
         if cfg.out.mode == "file":
             # write out responses to csv file
             # this is appropriate for short llm responses, in which case list `writeout` is empty
             df = pl.DataFrame(records)
-            df.write_csv(cfg.out.csv_file, separator = cfg.out.csv_file_separator)
-            
-            # if writeout content is present, write the contents to a separate file (as it does not fit into the csv file)  
+            df.write_csv(cfg.out.csv_file, separator=cfg.out.csv_file_separator)
+
+            # if writeout content is present, write the contents to a separate file (as it does not fit into the csv file)
             # writeout means: cfg.out.response_to_file_length_threshold has been surpassed,
             # therefore the llm response is written out to a separate file, not into
             # the csv file.
@@ -93,7 +112,7 @@ def main(config_file: Annotated[Path, typer.Option("-c", "--config", exists=True
                     writeout_file = cfg.out.csv_file.parent / w.get("file")
                     writeout_file_best = bestfile(writeout_file)
                     writeout_file_best.write_text(w.get("content"), encoding="utf-8")
-                    
-    
+
+
 def cli():
     app()
--- src/oneshot/llm_request.py
+++ src/oneshot/llm_request.py
@@ -5,53 +5,61 @@ from oneshot.tables import csv_row_iterator
 from collections.abc import Iterator
 import re
 
+
 # A convenience class to hold requests to the LLM
 @dataclass
 class LLMRequest:
     url: str
     headers: dict[str]
     json: dict[str]
-    
+
+
 @dataclass
 class ImageInput:
     mimetype: str
     b64: str
 
+
 # Methods to generate LLMRequest objects
 
+
 #   openai
-def request_openai(model_name: str,
-                   question: str,
-                   instructions: str|None = None,
-                   context: str|None =  None,
-                   images: list[ImageInput] | None = None,
-                   api_key: str|None = None,
-                   url: str|None = "https://api.openai.com/v1/responses",
-                   temperature: float = 0.0) -> LLMRequest:
+def request_openai(
+    model_name: str,
+    question: str,
+    instructions: str | None = None,
+    context: str | None = None,
+    images: list[ImageInput] | None = None,
+    api_key: str | None = None,
+    url: str | None = "https://api.openai.com/v1/responses",
+    temperature: float = 0.0,
+) -> LLMRequest:
     # header contains the api key
     headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
-    
+
     # if context is provided, place between <context></context> tags
     context_x = f"<context>\n{context}\n</context>" if context else None
-    
+
     # place instructions, context, and question into input_text
     content = [
         {
             "type": "input_text",
-            "text": f"{instructions or ''}\n{context_x or ''}\n{question}"
+            "text": f"{instructions or ''}\n{context_x or ''}\n{question}",
         }
     ]
-    
+
     # if images are provided, place into content as "input_image"; append each image separately
     if images:
         for img in images:
-            content.append({
-                "type": "input_image",
-                "image_url": f"data:{img.mimetype};base64,{img.b64}"
-            })
-    
+            content.append(
+                {
+                    "type": "input_image",
+                    "image_url": f"data:{img.mimetype};base64,{img.b64}",
+                }
+            )
+
     # put everything into json payload
-    json={
+    json = {
         "model": f"{model_name}",
         "temperature": temperature,
         "input": [
@@ -61,49 +69,54 @@ def request_openai(model_name: str,
             }
         ],
     }
-    
+
     # return url, headers, and json payload
     return LLMRequest(url=url, headers=headers, json=json)
 
+
 # ollama
-def request_ollama(model_name: str,
-                   question: str,
-                   instructions: str|None = None,
-                   context: str|None =  None,
-                   images: list[ImageInput] | None = None,
-                   url: str|None = None,
-                   temperature: float = 0.0) -> LLMRequest:
+def request_ollama(
+    model_name: str,
+    question: str,
+    instructions: str | None = None,
+    context: str | None = None,
+    images: list[ImageInput] | None = None,
+    url: str | None = None,
+    temperature: float = 0.0,
+) -> LLMRequest:
     if not url:
         raise ValueError("Argument 'url' must be supplied!")
-    headers = {} # empty by default, actually not needed
-    
+    headers = {}  # empty by default, actually not needed
+
     # if context is provided, place between <context></context> tags
     context_x = f"<context>\n{context}\n</context>" if context else None
     # place instructions, context, and question into prompt
     prompt = f"{instructions or ''}\n{context_x or ''}\n{question}"
-    json = {"model": model_name,
-            "prompt": prompt,
-            "stream": False,
-            "options": {
-                "temperature": temperature
-            }}
+    json = {
+        "model": model_name,
+        "prompt": prompt,
+        "stream": False,
+        "options": {"temperature": temperature},
+    }
     # if images present, place into json under heading "images"
     if images:
         json["images"] = [img.b64 for img in images]
-    
+
     # return url, headers (empty), and json payload
-    return LLMRequest(url = url, headers = headers, json = json) 
+    return LLMRequest(url=url, headers=headers, json=json)
+
 
 # Request function dictionary containing functions that return LLMRequest objects
 REQUESTFUN = {"openai": request_openai, "ollama": request_ollama}
+
 
 # `process_config` is a function that takes a config object and returns an iterator yielding LLMRequest objects
 def process_config(cfg: Config) -> Iterator[tuple[str, LLMRequest]]:
     # function for generating LLMRequest objects
     reqfun = REQUESTFUN[cfg.query.target]
-    
+
     # singleton-image
-    if (cfg.query.type == "singleton-image"):
+    if cfg.query.type == "singleton-image":
         # package images, which are in "image" and "ref_imgs"
         images = []
         # if ref_imgs present, place those first into images
@@ -111,57 +124,72 @@ def process_config(cfg: Config) -> Iterator[tuple[str, LLMRequest]]:
             for ri in cfg.query.details.ref_imgs:
                 images.append(ImageInput(mimetype=guess_image_mime(ri), b64=b64enc(ri)))
         # finally place "image" into "images"
-        images.append(ImageInput(guess_image_mime(cfg.query.details.image), b64=b64enc(cfg.query.details.image)))
-        
+        images.append(
+            ImageInput(
+                guess_image_mime(cfg.query.details.image),
+                b64=b64enc(cfg.query.details.image),
+            )
+        )
+
         # create arguments shared by ollama and openai
-        args = {"images": images,
-                "question": pth(cfg.query.details.question),
-                "instructions": pth(cfg.query.details.instructions),
-                "model_name": cfg.query.model_name,
-                "temperature": cfg.query.temperature}
-        
-        if (cfg.query.target == "ollama"):
+        args = {
+            "images": images,
+            "question": pth(cfg.query.details.question),
+            "instructions": pth(cfg.query.details.instructions),
+            "model_name": cfg.query.model_name,
+            "temperature": cfg.query.temperature,
+        }
+
+        if cfg.query.target == "ollama":
             # create ollama-specific arguments for reqfun
             args.update({"url": cfg.vendor.ollama.url})
-        elif (cfg.query.target == "openai"):
+        elif cfg.query.target == "openai":
             # create openai-specific arguments for reqfun
             args.update({"api_key": cfg.vendor.openai.api_key})
         else:
             # this should never happen, but let's be safe:
-            return ValueError(f"Target {cfg.query.target} is currently not implemented.")
+            return ValueError(
+                f"Target {cfg.query.target} is currently not implemented."
+            )
         # pass the arguments to reqfun
         yield "singleton", reqfun(**args)
-    
+
     # singleton-text
-    elif (cfg.query.type == "singleton-text"):
+    elif cfg.query.type == "singleton-text":
         # create arguments shared by openai and ollama
-        args = {"question": pth(cfg.query.details.question),
-                "context": pth(cfg.query.details.context),
-                "instructions": pth(cfg.query.details.instructions),
-                "model_name": cfg.query.model_name,
-                "temperature": cfg.query.temperature}
-        
-        if (cfg.query.target == "ollama"):
+        args = {
+            "question": pth(cfg.query.details.question),
+            "context": pth(cfg.query.details.context),
+            "instructions": pth(cfg.query.details.instructions),
+            "model_name": cfg.query.model_name,
+            "temperature": cfg.query.temperature,
+        }
+
+        if cfg.query.target == "ollama":
             # create ollama-specific arguments for reqfun
             args.update({"url": cfg.vendor.ollama.url})
-        elif (cfg.query.target == "openai"):
+        elif cfg.query.target == "openai":
             # create openai-specific arguments for reqfun
             args.update({"api_key": cfg.vendor.openai.api_key})
         else:
             # this should never happen, but let's be safe:
-            return ValueError(f"Target {cfg.query.target} is currently not implemented.")
+            return ValueError(
+                f"Target {cfg.query.target} is currently not implemented."
+            )
         # pass the arguments to reqfun
         yield "singleton", reqfun(**args)
-        
+
     # batch-image
-    elif (cfg.query.type == "batch-image"):
+    elif cfg.query.type == "batch-image":
         # determine the file iterator:
         #   default:            all the files in the directory
-        #   if glob supplied:   only the files corresponding to glob pattern    
+        #   if glob supplied:   only the files corresponding to glob pattern
         img_files_it = cfg.query.details.img_dir.iterdir()
         if cfg.query.details.img_dir_glob:
-            img_files_it = cfg.query.details.img_dir.glob(cfg.query.details.img_dir_glob)
-        
+            img_files_it = cfg.query.details.img_dir.glob(
+                cfg.query.details.img_dir_glob
+            )
+
         # loop over files
         for img_file in img_files_it:
             # loop over images
@@ -170,27 +198,35 @@ def process_config(cfg: Config) -> Iterator[tuple[str, LLMRequest]]:
             # if ref_imgs present, place those first into images
             if cfg.query.details.ref_imgs:
                 for ri in cfg.query.details.ref_imgs:
-                    images.append(ImageInput(mimetype=guess_image_mime(ri), b64=b64enc(ri)))
+                    images.append(
+                        ImageInput(mimetype=guess_image_mime(ri), b64=b64enc(ri))
+                    )
             # finally place current "img_file" into "images"
-            images.append(ImageInput(mimetype=guess_image_mime(img_file), b64=b64enc(img_file)))
-            
+            images.append(
+                ImageInput(mimetype=guess_image_mime(img_file), b64=b64enc(img_file))
+            )
+
             # pass on to ollama or openai
             # create arguments shared by openai and ollama
-            args = {"images": images,
-                    "question": pth(cfg.query.details.question),
-                    "instructions": pth(cfg.query.details.instructions),
-                    "model_name": cfg.query.model_name,
-                    "temperature": cfg.query.temperature}
-            
-            if (cfg.query.target == "ollama"):
+            args = {
+                "images": images,
+                "question": pth(cfg.query.details.question),
+                "instructions": pth(cfg.query.details.instructions),
+                "model_name": cfg.query.model_name,
+                "temperature": cfg.query.temperature,
+            }
+
+            if cfg.query.target == "ollama":
                 # create ollama-specific arguments for reqfun
                 args.update({"url": cfg.vendor.ollama.url})
-            elif (cfg.query.target == "openai"):
+            elif cfg.query.target == "openai":
                 # create openai-specific arguments for reqfun
                 args.update({"api_key": cfg.vendor.openai.api_key})
             else:
                 # this should never happen, but let's be safe:
-                return ValueError(f"Target {cfg.query.target} is currently not implemented.")
+                return ValueError(
+                    f"Target {cfg.query.target} is currently not implemented."
+                )
             # determine image qid
             image_qid = img_file.name
             if cfg.query.details.img_qid == "filename-regex":
@@ -198,33 +234,44 @@ def process_config(cfg: Config) -> Iterator[tuple[str, LLMRequest]]:
                 matches = p.findall(img_file.name)
                 if len(matches) > 0:
                     image_qid = matches[0]
-            
+
             # pass the arguments to reqfun
             yield image_qid, reqfun(**args)
-            
+
     # batch-text
-    elif (cfg.query.type == "batch-text"):
+    elif cfg.query.type == "batch-text":
         # here rows are taken from a csv file and looped over
-        for row in csv_row_iterator(cfg.query.details.csv_file):
+        for row in csv_row_iterator(
+            cfg.query.details.csv_file, separator=cfg.query.details.csv_file_separator
+        ):
             # loop over rows
             # create arguments shared by openai and ollama
-            args = {"question": pth(row.get(cfg.query.details.colname_questions, '')),
-                    "context": pth(row.get(cfg.query.details.colname_contexts, '')),
-                    "instructions": pth(row.get(cfg.query.details.colname_instructions, '')),
-                    "model_name": cfg.query.model_name}
-            
-            if (cfg.query.target == "ollama"):
+            args = {
+                "question": pth(row.get(cfg.query.details.colname_questions, "")),
+                "context": pth(row.get(cfg.query.details.colname_contexts, "")),
+                "instructions": pth(
+                    row.get(cfg.query.details.colname_instructions, "")
+                ),
+                "model_name": cfg.query.model_name,
+            }
+
+            if cfg.query.target == "ollama":
                 # create ollama-specific arguments for reqfun
                 args.update({"url": cfg.vendor.ollama.url})
-            elif (cfg.query.target == "openai"):
+            elif cfg.query.target == "openai":
                 # create openai-specific arguments for reqfun
                 args.update({"api_key": cfg.vendor.openai.api_key})
             else:
                 # this should never happen, but let's be safe:
-                return ValueError(f"Target {cfg.query.target} is currently not implemented.")
+                return ValueError(
+                    f"Target {cfg.query.target} is currently not implemented."
+                )
             # pass the arguments to reqfun
-            yield row.get(cfg.query.details.colname_query_id, 'no query id'), reqfun(**args)
-            
+            yield (
+                row.get(cfg.query.details.colname_query_id, "no query id"),
+                reqfun(**args),
+            )
+
     else:
         # this should never happen, but let's be safe:
         return ValueError(f"Query type {cfg.query.type} is currently not implemented.")
